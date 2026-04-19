1807d8b431ece7b7beb9accf3e7425078bc0dcb7
ninko-cc <264525673+ninko-cc@users.noreply.github.com>
2026-04-19 11:44:07 +0900

    feat: hide inactive timer button based on current phase

--- main.js
+++ main.js
@@ -65,9 +65,8 @@ async function start(time) {
 }
 
 function timeup(initTime) {
-  startFiveButtonEl.hidden = startTwentyFiveButtonEl.hidden = false;
-  startFiveButtonEl.disabled = initTime == FIVE_MIN;
-  startTwentyFiveButtonEl.disabled = initTime == TWENTY_FIVE_MIN;
+  startFiveButtonEl.hidden = initTime == FIVE_MIN;
+  startTwentyFiveButtonEl.hidden = initTime == TWENTY_FIVE_MIN;
   pauseButtonEl.hidden = true;
   resumeButtonEl.hidden = true;
 
